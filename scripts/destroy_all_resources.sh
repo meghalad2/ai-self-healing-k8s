fcf1e746e7179a99bf5deff7fede5b1b2d49b0f9
Megha <meghalad209@gmail.com>
2026-05-31 18:32:06 +0530

    removed the AWS secret

--- scripts/destroy_all_resources.sh
+++ scripts/destroy_all_resources.sh
@@ -6,8 +6,8 @@
 # 1. AWS Credentials Configuration
 # -------------------------------------------------------------
 # Re-authenticating terminal session using your active IAM credentials
-export AWS_ACCESS_KEY_ID="AKIAU6GDZAX3QTHN6V2F"
-export AWS_SECRET_ACCESS_KEY="gPWyY39jQ/zL8EeVYokKGvwy0CBfVp63yTUPVMRl"
+export AWS_ACCESS_KEY_ID="your_access_key_id_here"
+export AWS_SECRET_ACCESS_KEY="your_secret_access_key_here"
 export AWS_DEFAULT_REGION="us-east-1"
 
 echo "=========================================================="
